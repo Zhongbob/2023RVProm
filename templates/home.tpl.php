d29d0385d5cf0b81085acb8a05aa391f21192de1
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2023-11-18 21:26:50 +0800

    mostly done with about page
    left with animations & mobile view

--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -2,66 +2,76 @@
 </head>
 
 <body style="height: 100%;">
-    <section class = "hero">
-        <img src = "static/assets/about/logo.png">
+    <section class="hero">
+        <img src="static/assets/about/logo.png">
         <h1>Carpe Nocterm</h1>
         <h2>2023 JC 2 Prom</h2>
-        <button class = "default-button">Sign In</button>
+        <button class="default-button">Sign In</button>
     </section>
-    <section class = "programme-flow">
-        <h2>Programme Flow</h2>
-        <img src = "static/assets/about">
-    </section>
-    <section class = "details">
-        <h2>Details</h2>
-        <div class = "details__datetime">
-            <img src = "static/assets/about/date.png">
-            <p class = "date">Monday, 4th December</p>
-            <p class = "time">7pm - 10.30pm</p>
-        </div>
-        <div class = "details__location">
-            <img class = "mt_hotel"src = "static/assets/about/mariottTangHotel.png">
-            <p class = "venue">Marriott Tang Hotel, Level 3</p>
-            <button class = "default-button">View On Google Maps</button>
-        </div>
-    </section>
-    <section class = "dress-code">
-        <h2>Dress Code</h2>
-        <img src = "static/assets/about">
-        <p class = "dress-code">Formal</p>
-    </section>
-    <section class = "menu">
-        <h2>Menu</h2>
-        <img src = "static/assets/about">
-        <p class = "menu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-    </section>
+    <div class="general-details">
+        <section class="programme-flow">
+            <h2>Programme Flow</h2>
+            <img src="static/assets/about">
+        </section>
+        <section class="details">
+            <h2>Details</h2>
+            <div class="details__datetime">
+                <img src="static/assets/about/date.png">
+                <p class="date">Monday, 4th December</p>
+                <p class="time">7pm - 10.30pm</p>
+            </div>
+            <div class="details__location">
+                <img class="mt_hotel" src="static/assets/about/mariottTangHotel.png">
+                <p class="venue">Marriott Tang Hotel, Level 3</p>
+                <button class="default-button">View On Google Maps</button>
+            </div>
+        </section>
+        <section class="dress-code">
+            <h2>Dress Code</h2>
+            <img src="static/assets/about">
+            <p class="dress-code-description">The dress code is <b>Smart Casual</b> <br><br> Stick to our colour theme! Wear blue and/or black outfits</p>
+            <img class = "dress-code-example" src="static/assets/about/dressCode.png">
+            <p class = "masquerade-description">It’s a Masquerade Ball! <b>Buy</b> or <b>Create</b> your own unique masks before the event!</p>
+            <img class = "masquerade-example" src = "static/assets/about/masquerade.png">
+            <video>
+                <source src="static/assets/about">
+            </video> 
+        </section>
+        <section class="menu">
+            <h2>Menu</h2>
+            <img src="static/assets/about">
+            <p class="menu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </section>
+    </div>
+    <div class="programmes">
+        <section class="storyline">
+            <h2>Storyline</h2>
+            <video>
+                <source src="static/assets/about">
+            </video>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit dolor nihil obcaecati pariatur labore
+                animi eius perferendis magnam sit nam dicta eligendi fugiat, numquam quos reprehenderit illum est quam
+                ut!</p>
+        </section>
 
-    <section class = "storyline">
-        <h2>Storyline</h2>
-        <video>
-            <source src = "static/assets/about">
-        </video>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit dolor nihil obcaecati pariatur labore animi eius perferendis magnam sit nam dicta eligendi fugiat, numquam quos reprehenderit illum est quam ut!</p>
-    </section>
+        <section class="scavenger-hunt">
+            <h2>Scavenger Hunt</h2>
+            <p class="desc1">The Mighty hero Parker Peter’s identity is at risk due to some inadvertently left clues!
+            </p>
+            <img class="img1" src="static/assets/about/scavengerHunt.png">
+            <div class="desc2">
+                <p>Scan the 15 QR codes hidden around the venue and perform a task to eliminate these clues before they
+                    fall into Eris's hands. Let's protect our hero's secret together!</p>
+                <button class="default-button">Scan</button>
+            </div>
+            <img class="img2" src="static/assets/about/venue.png">
+        </section>
 
-    <section class = "scavenger-hunt">
-        <h2>Scavenger Hunt</h2>
-        <p class = "desc1">The Mighty hero Parker Peter’s identity is at risk due to some inadvertently left clues! </p>
-        <img class = "img1" src = "static/assets/about/scavengerHunt.png">
-        <div class = "desc2">
-            <p >Scan the 15 QR codes hidden around the venue and perform a task to eliminate these clues before they fall into Eris's hands. Let's protect our hero's secret together!</p>
-            <button class = "default-button">Scan</button>
-        </div>
-        <img class = "img2" src = "static/assets/about">
-    </section>
-
-    <section class = "booth-games">
-        <h2>Booth Games</h2>
-        <img class = "img3" src = "static/assets/about">
-        <p class = "desc3">Join the mission to equip Peter Parker with vital medical supplies! Test your skills at our themed booth games and stand a chance to win 
-amazing prizes.</p>
-    </section>
-    
-
-
-            
+        <section class="booth-games">
+            <h2>Booth Games</h2>
+            <img class="img3" src="static/assets/about/boothGamePrizes.png">
+            <p class="desc3">Join the mission to equip Peter Parker with vital medical supplies! Test your skills at our
+                themed booth games and stand a chance to win
+                amazing prizes.</p>
+        </section>
+    </div>
